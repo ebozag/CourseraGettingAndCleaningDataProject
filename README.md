a2a858e3c879ec81846c0423efd6e2fb0f312799
Edwin Boza <eboza@espol.edu.ec>
2015-11-21 18:59:33 -0500

    EBG - Added comments and README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CourseraGettingAndCleaningDataProject
 CourseraGettingAndCleaningDataProject
+
+This repository contains my code for the Coursera's Getting and Cleaning Data Course Project.
+
+The main program "run_analysis.R" takes the dataset containing data collected from the accelerometers from the Samsung Galaxy S smartphone, and process it to obtain a new tidy dataset with the mean values for each activity and subject in the original dataset. From the original Variables we only take those corresponding to "mean"s and "std"s.
+
+The R program has the following sections:
+
+- Check if the dataset is present, otherwise it will download and unzip the corresponding file.
+- Read the files with Activity Labels and Features, these will be use to replace Variable names and activity values with descriptive strings.
+- Read the files with datasets for "training" and "testing". Merge data from different files (measurements, subject id, and activity), and apply the descriptive activity labels.
+- Merge both datasets: testing and training.
+- Rename the columns in the dataset with descriptive Variable names.
+- Select only columns for Subject id, Activity, and features with Mean and STD
+- Group the dataset by Activity and Subject and create a new tody dataset with the mean values for each variable (column).
+- Write the new tidy dataset to a file.
+
+The name of the variables are listed in the CodeBook.md file included in this repository.
--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,14 @@
+###############################
+### This code is the Getting and Cleaning Data Course Project
+### 
+### Student:  Edwin F. Boza
+### Date:     21/Nov/2015
+### 
+
 ### Load libraries
 library(dplyr)
-library(tidyr)
 
-### Download file if needed
+### Download file if needed, and unzip it.
 datasetFile <- "UCI-HAR-Dataset.zip"
 if (!file.exists(datasetFile)){
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", datasetFile, method="curl")
@@ -11,18 +17,20 @@ if (!file.exists("UCI HAR Dataset")) {
  unzip(datasetFile) 
 }
    
-### Read the files with Activity Labels
+### Read the files with Activity Labels and Features
 ### Convert to tbl_dt
 dataTable <- read.table("UCI HAR Dataset/activity_labels.txt")
 activities <- tbl_dt(dataTable)
 dataTable <- read.table("UCI HAR Dataset/features.txt")
 features <- tbl_dt(dataTable)
+### Remove the () symbols and replace Mean and STD names.
 features <- mutate(features,V2 = gsub("[()]", "", V2))
 features <- mutate(features,V2 = gsub("-mean", "Mean", V2))
 features <- mutate(features,V2 = gsub("-std", "STD", V2))
 
 ### Read the files with datasets
 ### Convert to tbl_dt and join them
+### Descriptive activity labels are applied in this step.
 dataTable <- read.table("UCI HAR Dataset/train/X_train.txt")
 training <- tbl_dt(dataTable)
 dataTable <- read.table("UCI HAR Dataset/train/subject_train.txt")
@@ -40,14 +48,16 @@ testing <- cbind("Activity" = tbl_dt(activities$V2[dataTable$V1]), testing)
 ### Bind/Merge both datasets: testing and training
 dataset <- rbind(training,testing)
 
-### Rename the columns in the dataset
+### Rename the columns in the dataset with descriptive Variable names.
 colnames(dataset) <- c("Activity", "Subject", features$V2)
 
 ### Select only columns for Subject id, Activity, and features with Mean and STD
 dataset <- select(dataset, Subject, Activity, contains("Mean"), contains("STD"))
 
-### Group by Activity and Subject
+### Group the dataset by Activity and Subject
 byActivitySubject <- group_by(dataset, Activity, Subject)
+
+### Create the new tidy dataset with the mean for each variable (column)
 tidy <- summarise_each(byActivitySubject,funs(mean))
 
 ### Write tidy dataset to a file
